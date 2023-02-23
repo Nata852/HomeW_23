20b6cf70fbf0c44145760ecd5d7a5b3489bedd29
Natali <natlyly3@mail.ru>
2023-02-23 12:08:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+  {
+    Console.WriteLine(message);
+    if(int.TryParse(Console.ReadLine(), out result) && result>0)
+   {
+    break;
+   }
+    else
+   {
+    Console.WriteLine("Ввели не число. Повторите ввод ");
+   }
+}
+return result;
+}
+void GetCube(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{i*i*i}, ");
+    }
+}
+int n=GetNumber("Введите число N: ");
+GetCube(n);
